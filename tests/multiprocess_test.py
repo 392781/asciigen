59add437b8cab2cd2f4752d1a31e2651033b780f
392781 <rlencevicius@gmail.com>
2020-08-12 22:38:02 -0700

    Fixed the pixel access time... 20x faster

--- tests/multiprocess_test.py
+++ tests/multiprocess_test.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageStat
 from collections import defaultdict
 import multiprocessing as mp
 from functools import partial
@@ -44,13 +44,54 @@ def find_dictionary(font, size):
         
         return ascii_gradient
 
+def _find_dictionary(font, size):
+    font = ImageFont.truetype(font, size)
+    ascii_gradient = defaultdict(list)
+    #creates map with a list of values
+    table = defaultdict(list)
+    table[0].append(chr(32))
+    min = 0
+    max = 0
+    
+    for i in range(33,127):
+        # finds the characters height and width and creates an 
+        # image where it pastes the character and processes it
+        h,w = font.getsize(chr(i))
+        image = Image.new("RGB", (h, w))
+        draw = ImageDraw.Draw(image)
+        draw.text((0,0), chr(i), font=font)
+        image = image.convert('L')
+        stats = ImageStat.Stat(image)
+        
+        # finds the brightness value of the character and adds 
+        # it to the table
+
+        sum = stats.sum[0]
+                
+        brightness_value = int(sum/(h*w))
+        
+        table[brightness_value].append(chr(i))
+
+        if (brightness_value > max):
+            max = brightness_value
+
+    for key in table:
+        tmp = int((255 * (key - min)) / (max - min))
+        ascii_gradient[tmp] = table[key]
+    
+    return ascii_gradient
+
 def main():
     # Let's try to find the longest possible dictionary of values to use as a gradient (longer dictionary = more detail)
 
     # This function will go through each font size individually as a single process on a single CPU core
-    def singleproc():
-        for i in range(8, 131):
-            find_dictionary('../fonts/FSEX300.ttf', i)
+    def singleproc(naive):
+        if (naive == True):
+            for i in range(8, 131):
+                find_dictionary('../fonts/FSEX300.ttf', i)
+        else:
+            for i in range(8, 131):
+                _find_dictionary('../fonts/FSEX300.ttf', i)
 
     # Pool will let us use multiple cores on a CPU (4 on mine) to split up our search for a gradient.  
     # in my case about 1/4th of the font sizes will go to each core and run the find_dictionary func simultaneously
@@ -60,6 +101,7 @@ def main():
     # With the font parameter set, we just need to give an iterable range of sizes for our function and run it in parallel!
     # Let's also time it :D
     print('Let\'s start!')
+    print('Naive == True')
     start = timer()
     pool.map(func_partial, iter(range(8,131)))
     end = timer()
@@ -67,9 +109,32 @@ def main():
 
     # Let's time the single core
     start = timer()
-    singleproc()
+    singleproc(True)
     end = timer()
     print('Using 1 core: {:>13.5}s'.format(end - start))
+
+    # ----------------------------------------------------
+
+    # Pool will let us use multiple cores on a CPU (4 on mine) to split up our search for a gradient.  
+    # in my case about 1/4th of the font sizes will go to each core and run the find_dictionary func simultaneously
+    pool = mp.Pool(mp.cpu_count())
+    # This bit below gives a constant parameter for the font name in our find_dictionary function.
+    func_partial = partial(_find_dictionary, '../fonts/FSEX300.ttf')
+    # With the font parameter set, we just need to give an iterable range of sizes for our function and run it in parallel!
+    # Let's also time it :D
+    print('Let\'s start!')
+    print('Naive == False')
+    start = timer()
+    pool.map(func_partial, iter(range(8,131)))
+    end = timer()
+    print('Using {} core(s): {:>10.5}s'.format(mp.cpu_count(), end-start))
+
+    # Let's time the single core
+    start = timer()
+    singleproc(False)
+    end = timer()
+    print('Using 1 core: {:>13.5}s'.format(end - start))
+
 
 if (__name__ == '__main__'):
     main()
