3fcf13070a22ac3ed36b2672e156835fcd1a2d42
392781 <rlencevicius@gmail.com>
2020-08-12 22:15:57 -0700

    Updated gitignore

--- asciigen/api.py
+++ asciigen/api.py
@@ -13,6 +13,129 @@ asciigen - param : font
     generate - param : image, original_size, generate_size, color
 '''
 
+_gradient_dictionary = defaultdict(list)
+
+def _find_dictionary(font, size):
+        font = ImageFont.truetype(font, size)
+        ascii_gradient = defaultdict(list)
+        #creates map with a list of values
+        table = defaultdict(list)
+        table[0].append(chr(32))
+        min = 0
+        max = 0
+        
+        for i in range(33,127):
+            # finds the characters height and width and creates an 
+            # image where it pastes the character and processes it
+            h,w = font.getsize(chr(i))
+            image = Image.new("RGB", (h, w))
+            draw = ImageDraw.Draw(image)
+            draw.text((0,0), chr(i), font=font)
+            image = image.convert('L')
+            
+            # finds the brightness value of the character and adds 
+            # it to the table
+
+            sum = 0
+            for x in range(h - 1):
+                for y in range(w - 1):
+                    mu = image.getpixel((x,y))
+                    sum += mu
+                    
+            val = int(sum/(h*w))
+            
+            table[val].append(chr(i))
+
+            if (val > max):
+                max = val
+
+        for key in table:
+            tmp = int((255 * (key - min)) / (max - min))
+            ascii_gradient[tmp] = table[key]
+        
+        return ascii_gradient
+
+
+def _select_symbol(dictionary, sorted_keys, val):
+    gradient_idx = bisect(sorted_keys, val) - 1
+    key = sorted_keys[gradient_idx]
+    symbols = dictionary[key]
+    if (len(symbols) > 1):
+        idx = r.randint(0, len(vals) - 1)
+        return symbols[idx]
+    else:
+        return symbols[0]
+
+
+def print_dict(dictionary):
+    for key in sorted(dictionary.keys()):
+        print(key, dictionary[key])
+
+
+def length(dictionary):
+    return len(dictionary)
+
+
+def generate(font, fontsize, image, gradient = ' .:+/$@'):
+    """
+    Creates an ASCII image using a predefined lookup table
+    
+    Uses predefined 'ascii' string and 'breakpoints' list to then test
+    the brightness of each pixel in the image and uses the breakpoints 
+    to determine which value to choose from the ascii string.  The 
+    chosen character is then appended to the string with linebreaks 
+    between each subsequent line of pixels
+    
+    Parameters:
+        image (image):    Image to be processed and generated
+    
+    Returns:
+        image (image):    Image representing the original input as ascii
+    """
+
+    if (gradient is str):
+        ascii_table = gradient.split('')
+    elif (gradient is list):
+        ascii_table = gradient
+
+    ascii_table.printdict()
+    w,h = image.size[0], image.size[1]
+    x = gcd(h, w)
+    h //= x
+    w //= x
+    h, w = 12, 7
+    print(h,w)
+    left_px = 0
+    xp = 0
+    yp = 0
+    brightness = 0
+    string = ""
+    while (h + yp < image.size[1]):
+        while (w + xp < image.size[0]):    
+            for y in range(yp, h + yp):
+                for x in range(xp, w + xp):
+                    brightness += image.getpixel((x, y))
+            xp += w 
+            brightness = brightness // (h*w)
+            char = ascii_table.select_symbol(brightness)
+            string = string + char      
+        xp = 0
+        yp += h
+        string = string + "\n"
+                
+    size = 10000
+    im = Image.new("RGB", (size,size))
+    img = ImageDraw.Draw(im)
+    font = ImageFont.truetype(font, fontsize)
+    img.text((0,0), string, font = font)
+    
+    im = im.crop(im.getbbox())
+    im = im.convert('1')
+    im = im.resize((im.size[0]//8, im.size[1]//8))
+    im.show()
+    return im
+
+
 class asciigen:
     '''
     Parameters
